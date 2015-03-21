baba0eda371a2a842330018f5f12481d942ce837
Scott Love <scottlove@unboxedtechnology.com>
2015-03-21 11:59:55 -0400

    Update to Chrome driver and using Py unittest framework

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
     def test_can_start_a_list_and_retrieve_it_later(self):
